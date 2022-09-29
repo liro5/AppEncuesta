24e71e31801f9bf833854896dd954d6b556f9751
liro5 <lidisrondon@yahoo.es>
2022-09-28 20:05:47 -0500

    Agregue el nombre de Aleixer

---
+++ src/app/datos-encuesta/datos-encuesta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-encuesta',
+  templateUrl: './datos-encuesta.component.html',
+  styleUrls: ['./datos-encuesta.component.css']
+})
+export class DatosEncuestaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
