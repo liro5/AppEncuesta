6cb601a757c84332a64d6fec29ab0e46fbb1fcf4
Tatiana <gitalosu@gmail.com>
2022-09-28 20:53:58 -0500

    Carde Emojis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatosEncuestaComponent } from './datos-encuesta/datos-encuesta.component';
+import { PreguntasComponent } from './preguntas/preguntas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatosEncuestaComponent
+    DatosEncuestaComponent,
+    PreguntasComponent
   ],
   imports: [
     BrowserModule,
